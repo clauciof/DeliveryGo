dfe119a80b61c2c7533404d4537aebde81078ae0
clauciof <claucio2@gmail.com>
2019-02-04 21:52:13 -0200

    "criando Edição do Perfil"

---
+++ app/src/main/java/com/clauceta/deliverygo/activitys/EditaPerfilActivity.kt
@@ -0,0 +1,21 @@
+package com.clauceta.deliverygo.activitys
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.clauceta.deliverygo.R
+
+class EditaPerfilActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.edita_perfil)
+
+    }
+
+
+
+
+
+
+}
