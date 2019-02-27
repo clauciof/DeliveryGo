c627512921f0a90d1fcd8f16abb7a0a75b442182
clauciof <claucio2@gmail.com>
2019-02-27 20:00:22 -0300

    Uploading de imagens para o Firebase

--- app/src/main/java/com/clauceta/deliverygo/activitys/EditaPerfilActivity.kt
+++ app/src/main/java/com/clauceta/deliverygo/activitys/EditaPerfilActivity.kt
@@ -6,21 +6,35 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
+import android.widget.ProgressBar
+import android.widget.Toast
 import com.bumptech.glide.request.RequestOptions
 import com.clauceta.deliverygo.R
 import com.clauceta.deliverygo.config.GlideApp
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.edita_perfil.*
 import kotlinx.android.synthetic.main.header.*
 
 class EditaPerfilActivity: AppCompatActivity() {
 
+    // retorna id unico do usuario logado
+    var id_current_user = FirebaseAuth.getInstance().currentUser!!.uid
+
+    // referencia o Storage do Firebase
+    var fotoStorage = FirebaseStorage.getInstance().getReference("UsersPhotos")
+
 
     var caminhoFoto:String? = null //salva o caminho da foto tirada
-    var caminhoFotoAceita:String? = null //salva o caminho da foto aceita pelo usuário
+    var path: Uri? = null
+
 
     companion object {
         private const val REQUEST_CAMERA: Int = 1
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +44,18 @@ class EditaPerfilActivity: AppCompatActivity() {
         icone_mudar_foto_perfil.setOnClickListener{
             carregaFoto()
         }
+
+
+        botao_salvar_editar_perfil.setOnClickListener {
+
+
+            var caminhoFotoAceita: StorageReference  //salva o caminho da foto aceita pelo usuário e joga no backend
+
+            caminhoFotoAceita = fotoStorage.child(id_current_user).child(path!!.lastPathSegment.toString())
+
+            caminhoFotoAceita.putFile(path!!)
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -39,7 +65,6 @@ class EditaPerfilActivity: AppCompatActivity() {
         //exibe a foto retornada pelo aplicativo de câmera
         if(requestCode == EditaPerfilActivity.REQUEST_CAMERA && resultCode == Activity.RESULT_OK){
 
-            var path: Uri
 
             path = data!!.data
 
@@ -51,9 +76,8 @@ class EditaPerfilActivity: AppCompatActivity() {
                     .centerCrop()
                     .apply(RequestOptions().circleCrop())
                     .into(icone_mudar_foto_perfil)
-
-            caminhoFotoAceita = caminhoFoto
         }
+
     }
 
 
